52b9ff53ad40397e2ae23a038532ac9364b2cb12
kim0504 <81956540+kim0504@users.noreply.github.com>
2023-07-11 16:57:14 +0900

    globals 사용

--- structure.py
+++ structure.py
@@ -1,65 +1,50 @@
 import gradio as gr
-import openai
+import ui, func
 
 ex_fairy = [{"image":"","title":"제목입니다", "content":"내용입니다"},
+            {"image":"","title":"제목입니다", "content":"내용입니다"},
+            {"image":"","title":"제목입니다", "content":"내용입니다"},
+            {"image":"","title":"제목입니다", "content":"내용입니다"},
             {"image":"","title":"제목입니다", "content":"내용입니다"}]
 
 with gr.Blocks() as demo:
-
     with gr.Column() as init:
-        logo = gr.Image()
-        with gr.Row():
-            new_btn = gr.Button("New")
-            load_btn = gr.Button("Load")
-            option_btn = gr.Button("Option")
+        globals().update(ui.init_dis())
 
     with gr.Column(visible=False) as new_list:
-        start_btn1 = gr.Button("Home")
-        for fairy in ex_fairy:
-            with gr.Row():
-                fairy_image = gr.Image()
-                with gr.Column():
-                    fairy_title = gr.Textbox(fairy['title'])
-                    fairy_content = gr.Textbox(fairy['content'])
-                select_btn = gr.Button("선택")
+        globals().update(ui.new_list_dis(ex_fairy))
+
+    with gr.Column(visible=False) as new:
+        globals().update(ui.new_dis())
 
     with gr.Column(visible=False) as load_list:
-        start_btn1 = gr.Button("Home")
-        for fairy in ex_fairy:
-            with gr.Row():
-                fairy_image = gr.Image()
-                with gr.Column():
-                    fairy_title = gr.Textbox(fairy['title'])
-                    fairy_content = gr.Textbox(fairy['content'])
-                select_btn = gr.Button("선택")
+        globals().update(ui.load_list_dis(ex_fairy))
 
-    with gr.Column(visible=False) as main:
-        start_btn2 = gr.Button("Home")
-        with gr.Row():
-            img = gr.Image()
-            content = gr.Textbox()
-        with gr.Column():
-            with gr.Row():
-                btn1 = gr.Button("1")
-                btn2 = gr.Button("2")
-            with gr.Row():
-                btn3 = gr.Button("3")
-                btn4 = gr.Button("4")
+    with gr.Column(visible=False) as load:
+        globals().update(ui.load_dis())
 
-    def move_main():
-        return gr.update(visible=False), gr.update(visible=False), gr.update(visible=True)
+    pages = [init, new_list, new, load_list, load]
 
-    def move_list():
-        return gr.update(visible=False), gr.update(visible=True), gr.update(visible=False)
+    home_btn = [key for key, value in globals().items() if key.endswith('home_btn')]
+    for btn in home_btn:
+        globals()[btn].click(func.move_init, [], pages)
 
-    def move_init():
-        return gr.update(visible=True), gr.update(visible=False), gr.update(visible=False)
+    new_select_btn = [key for key, value in globals().items() if key.startswith('new_list_select_btn')]
+    for btn in new_select_btn:
+        globals()[btn].click(func.move_new, [], pages)
 
-    new_btn.click(move_list, [], [init, new_list, main])
-    select_btn.click(move_main, [], [init, new_list, main])
-    start_btn1.click(move_init, [], [init, new_list, main])
-    start_btn2.click(move_init, [], [init, new_list, main])
+    load_select_btn = [key for key, value in globals().items() if key.startswith('load_list_select_btn')]
+    for btn in load_select_btn:
+        globals()[btn].click(func.move_load, [], pages)
 
+    select_btn = [key for key, value in globals().items() if key.startswith('new_select_btn')]
+    for btn in select_btn:
+        pass
 
-if __name__ == "__main__":
-    demo.launch()
+    globals()['new_list_select_btn1'].click(func.move_new, [], pages)
+    globals()['load_list_select_btn1'].click(func.move_load, [], pages)
+
+    globals()['new_btn'].click(func.move_new_list, [], pages)
+    globals()['load_btn'].click(func.move_load_list, [], pages)
+
+demo.launch()
